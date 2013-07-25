d735bedc2d7e9d068e8a8adb49298da094b85b04
Suraj Duggirala <surajdugg@gmail.com>
2013-07-25 13:55:53 +0000

    Added hello from index

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
+var data = fs.readFileSync('./index.html','utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
